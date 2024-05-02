f0f62c1e1fa18f4806b35ff8f6bcaf9b00286568
Nolann Boyere <nolann.boyere@etu.univ-nantes.fr>
2024-05-02 12:28:18 +0200

    gras simplifié

--- Gras.java
+++ Gras.java
@@ -1,18 +1,15 @@
 import java.awt.BasicStroke;
 import java.awt.Graphics2D;
-
 public class Gras implements Figure {
     Figure f;
-    BasicStroke s;
 
     Gras(Figure _f) {
         f = _f;
-        s = new BasicStroke(5);
     }
     
     @Override
     public void draw(Graphics2D g) {
-        g.setStroke(s);
+        g.setStroke(new BasicStroke(5));
         f.draw(g);
     }
 }
