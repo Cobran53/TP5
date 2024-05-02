1aa04af7e556c16aab7b7c60153c4caaa1f3cb05
MaxTCH44 <tch.maxime@gmail.com>
2024-05-02 11:40:56 +0200

    ohiiui

--- Cercle.java
+++ Cercle.java
@@ -1,6 +1,6 @@
 import java.awt.Graphics2D ;
 
-public class Cercle implements Figure {
+public class Cercle implements Figure { //on reprend le modèle du rectangle en remplaçant la hauteur et la largeur par le diamètre
     int x  ;
     int y  ;
 
@@ -13,6 +13,6 @@ public class Cercle implements Figure {
     }
 
     public void draw(Graphics2D g){
-        g.drawOval(x,y,r,r);
+        g.drawOval(x,y,r,r);    // au lieu de tracer un rectangle, on trace un ovale avec le diamètre pour la hauteur et la largeur de l'ovale ce qui nous donne un cercle
     }
 }
--- Groupe.java
+++ Groupe.java
@@ -1,15 +1,15 @@
 import java.awt.Graphics2D;
 
-public class Groupe implements Figure {
+public class Groupe implements Figure { // le groupe implémente l'interface Figure ce qui fait de lui une Figure aussi
     
-    Figure a, b;
+    Figure a, b;                        // on recupère 2 figures
     Groupe(Figure a, Figure b) {
         this.a = a;
         this.b = b;
     }
 
     @Override
-    public void draw(Graphics2D g) {
+    public void draw(Graphics2D g) {    // on les dessine l'une après l'autre
         a.draw(g);
         b.draw(g);
     }
